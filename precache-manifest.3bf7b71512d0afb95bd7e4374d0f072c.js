fff431e6dd2d47c29819e8855c7da2b7c426d05b
junmindereal <junmindereal@gmail.com>
2020-02-07 15:45:13 +0800

    Updates

---
+++ precache-manifest.3bf7b71512d0afb95bd7e4374d0f072c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5506948040e2a5ad8a7e91e3c71bfb",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "862bae132c58860c1b7d",
+    "url": "/todo-app/static/js/2.cfbe782d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/todo-app/static/js/2.cfbe782d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dc760a38cd850d2e277",
+    "url": "/todo-app/static/js/main.5930d357.chunk.js"
+  },
+  {
+    "revision": "b66ecab6fc712c6d22a7",
+    "url": "/todo-app/static/js/runtime-main.e77e13dd.js"
+  }
+]);
